d67700dd8bde636eb29ca91c9c4d959239b7e870
RubenDav2112 <rubengithub2112@gmail.com>
2023-11-18 21:14:00 -0600

    agregue mis dos div del home

--- pages/homee.php
+++ pages/homee.php
@@ -4,10 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MAMBA - Home</title>
+    <link rel="stylesheet" href="../resources/css/estilosInicioRuben.css">
 </head>
 <body>
     <?php
         include('../includes/headr.html');
     ?>
+
+    <center>
+        <div class="contenedor">
+            <div id="imagen1"><img src="../resources/img/imgRuben/img12.png" alt="" id="imgIzquierda"></div>
+            <div id="imagen2"><img src="../resources/img/imgRuben/img11.jpeg" alt="" id="imgDerecha"></div>
+            <div id="imagen3"><img src="../resources/img/imgRuben/img3.jpg" alt="" class="imgAbajo"></div>
+            <div id="imagen4"><img src="../resources/img/imgRuben/img4.jpg" alt="" class="imgAbajo"></div>
+            <div id="imagen5"><img src="../resources/img/imgRuben/img5.jpg" alt="" class="imgAbajo"></div>
+            <div id="imagen6"><img src="../resources/img/imgRuben/img6.jpg" alt="" class="imgAbajo"></div>
+            <div id="imagen7"><img src="../resources/img/imgRuben/img7.jpg" alt="" class="imgAbajo"></div>
+        </div>
+    </center>
+
+
+    <div id="secDiv">
+        <table>
+            <tr>
+                <td class="tsecDiv"><h1>THE CURRENT <br> CULTURE <br> MARKETPLACE</h1></td>
+                <td rowspan="2"><img src="../resources/img/imgRuben/CONEJO.png" alt=""></td>
+            </tr>
+            <tr>
+                <td class="tsecDiv"><p>En Mamba Sneakers, nuestra misión es simple pero poderosa: ofrecerte tenis de alta calidad que no solo cumplan, sino que superen tus expectativas. Cada par que llevas no es solo moda, es una declaración de estilo y calidad. Camina con confianza, camina con Mamba</p></td>
+            </tr>
+        </table>
+        <div class="flexContenedor">
+            <div class="recDiv">
+                Verificacion  ++++
+            </div>
+            <div class="recDiv">
+                Compradores +++++
+            </div>
+            <div class="recDiv">
+                Vendedores  ++++
+            </div>
+        </div>
+    </div>
 </body>
 </html>
