43cae8c230e9ac25cf5118c1b8c43ddaa5fac436
RobFillingham <robertfillingham@hotmail.com>
2023-11-26 21:30:55 -0600

    Bugs

--- resources/php/incorrecto.php
+++ resources/php/incorrecto.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login incorrecto</title>
+    <title>Datos Erróneos</title>
     <link rel="stylesheet" href="../css/header.css">
     <style>
         body{
