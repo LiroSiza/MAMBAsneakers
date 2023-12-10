de3b9f890d8cf0ac0410feab73c93955645eb54d
LiroSiza <cesarreyestorres005@gmail.com>
2023-12-10 16:40:26 -0600

    solucion al responsive del header

--- includes/headrAdmin.php
+++ includes/headrAdmin.php
@@ -9,7 +9,6 @@
         <script src="https://kit.fontawesome.com/0d4c0ee316.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../resources/css/header.css">
         <link rel="icon" href="../resources/img/fav.ico" type="image/x-icon">
-        <script src="../resources/js/header.js"></script>
     </head>
     <body id="body">
         <nav class="nav-container">
@@ -132,7 +131,7 @@
                     
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+        <script src="../resources/js/header.js"></script>
         <!-- Este Script lo que hace es limpiar los campos del formulario cuando la ventana del modal se cierre  -->
         <script>
             $(document).ready(function(){
